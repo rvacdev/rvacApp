5fb28482f08575fb03d4ae1a1d73f277b8860f59
nspearsatu <148281455+nspearsatu@users.noreply.github.com>
2024-02-19 16:31:02 -0600

    Updated volunteer.dart to now work with the new signup page

--- volunteer.dart
+++ volunteer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import "package:rvac/signup_page.dart";
 
 final _formKey = GlobalKey<FormState>();
 
@@ -9,40 +10,67 @@ class Volunteer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Form(
-            key: _formKey,
-            child: Column(
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Enter a value";
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                hintText: 'Username',
+              ),
+            ),
+            TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Enter a value";
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                hintText: 'Password',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Great!'),
+                    ),
+                  );
+                }
+              },
+              child: const Text('validate'),
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextFormField(
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Enter a value";
-                    }
-                    return null;
+              children: <Widget>[
+                Text("Don't have an account? "),
+                GestureDetector(
+                  onTap: () {
+                    // Navigate to the signup page when "Sign Up" is tapped
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignUpPage()),
+                    );
                   },
+                  child: Text(
+                    "Sign Up",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 ),
-                TextFormField(
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Enter a value";
-                    }
-                    return null;
-                  },
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text('Great!'),
-                        ),
-                      );
-                    }
-                  },
-                  child: const Text('validate'),
-                )
               ],
-            )));
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
