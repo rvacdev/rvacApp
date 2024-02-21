1c6fa5b90da78155a6798c4d88ad1bb0a77ef798
nspearsatu <148281455+nspearsatu@users.noreply.github.com>
2024-02-21 16:20:50 -0600

    Update signup_page.dart header and more
    Added a header and changed some of the formatting to look cleaner(?) need to test on mobile devices

--- signup_page.dart
+++ signup_page.dart
@@ -1,108 +1,133 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'api.dart';
+import 'package:rvac/api.dart';
 
 class SignUpPage extends StatelessWidget {
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    String userEmail = ""; // Declares userEmail variable
-    String userName = ""; // Declares userName variable
-    String userPassword = ""; // Declares userPassword variable
+    String userEmail = "";
+    String userName = "";
+    String userPassword = "";
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Sign Up'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  hintText: 'Enter a valid Email Address',
-                  prefixIcon: Icon(Icons.email),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your email';
-                  }
-                  return null;
-                },
-                onChanged: (value) {
-                  userEmail = value; // Updates userEmail when the text changes
-                },
-              ),
-              SizedBox(height: 16.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Username',
-                  hintText: 'Enter a Username',
-                  prefixIcon: Icon(Icons.person),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your username';
-                  }
-                  return null;
-                },
-                onChanged: (value) {
-                  userName = value; // Updates userName when the text changes
-                },
-              ),
-              SizedBox(height: 16.0),
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  hintText: 'Enter a Password (min. 8 characters)',
-                  prefixIcon: Icon(Icons.lock),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your password';
-                  }
-                  if (value.length < 8) {
-                    return 'Password must be at least 8 characters long';
-                  }
-                  return null;
-                },
-                onChanged: (value) {
-                  userPassword = value; // Updates userPassword when the text changes
-                },
-              ),
-              SizedBox(height: 24.0),
-              ElevatedButton(
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
-                    final data = await Api.signUp(userEmail, userName, userPassword);
+      body: Center(
+        child: Container(
+          width: 300, // Set the width of the container to control the form's width
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Sign Up to Volunteer',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      hintText: 'Enter a valid Email Address',
+                      prefixIcon: Icon(Icons.email),
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20.0), // Adjust the horizontal padding
+                      isDense: true, // Reduce the vertical space
+                      labelStyle: TextStyle(fontSize: 16.0), // Adjust the font size of the label
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email';
+                      }
+                      return null;
+                    },
+                    onChanged: (value) {
+                      userEmail = value;
+                    },
+                  ),
+                  SizedBox(height: 20.0), // Add vertical space between fields
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                      hintText: 'Enter a Username',
+                      prefixIcon: Icon(Icons.person),
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20.0),
+                      isDense: true,
+                      labelStyle: TextStyle(fontSize: 16.0),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your username';
+                      }
+                      return null;
+                    },
+                    onChanged: (value) {
+                      userName = value;
+                    },
+                  ),
+                  SizedBox(height: 20.0), // Add vertical space between fields
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      hintText: 'Enter a Password (min. 8 characters)',
+                      prefixIcon: Icon(Icons.lock),
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20.0),
+                      isDense: true,
+                      labelStyle: TextStyle(fontSize: 16.0),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your password';
+                      }
+                      if (value.length < 8) {
+                        return 'Password must be at least 8 characters long';
+                      }
+                      return null;
+                    },
+                    onChanged: (value) {
+                      userPassword = value;
+                    },
+                  ),
+                  SizedBox(height: 24.0),
+                  ElevatedButton(
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        final data = await Api.signUp(
+                          userEmail,
+                          userName,
+                          userPassword,
+                        );
 
-                    if (data['success']) {
-                      // Signup successful
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text('Signup Successful!'),
-                        ),
-                      );
-
-                    } else {
-                      // Signup failed
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text('Signup Failed. Please try again.'),
-                        ),
-                      );
-                    }
-                  }
-                },
-                child: const Text('Sign Up'),
+                        if (data['success']) {
+                          // Signup successful
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('Signup Successful!'),
+                            ),
+                          );
+                        } else {
+                          // Signup failed
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('Signup Failed. Please try again.'),
+                            ),
+                          );
+                        }
+                      }
+                    },
+                    child: const Text('Sign Up'),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
